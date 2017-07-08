f2e4a2cc8112eac0dee5bdb989dac3658f2cf64b
Mykle <themykle@imykle.com>
2017-07-07 22:39:39 -0400

    Add files via upload

--- MMM-ImageHeader.js
+++ MMM-ImageHeader.js
@@ -7,20 +7,16 @@
 Module.register("MMM-ImageHeader", {
 
     defaults: {
-        headerImage: "YOUR_IMAGE_NAME", // Local Image name - Example: myPictureName.jpg
-		internetUrl: "",                // Only direct url to internet images
-		},
+        headerImage: "modules/MMM-ImageHeader/pix/YOUR_IMAGE_NAME", // local path to your image or internet url to image.
+    },
 
     start: function() {
         self = this;
-        this.url = "";
-	//  this.url = 'modules/MMM-ImageHeader/pix/' + this.config.headerImage;
+        this.url = '';
 
-        if (this.config.headerImage != "") {
-            this.url = "modules/MMM-ImageHeader/pix/" + this.config.headerImage;
-        } else {
-			this.url = this.config.internetUrl;
-	}
+        if (this.config.headerImage != '') {
+            this.url = this.config.headerImage;
+        }
     },
 
     getStyles: function() {
@@ -31,23 +27,10 @@ Module.register("MMM-ImageHeader", {
     getDom: function() {
         var wrapper = document.createElement("div");
         var image = document.createElement("img");
-		
-		if (this.config.headerImage != "") {
-			this.url = "modules/MMM-ImageHeader/pix/" + this.config.headerImage;
-		} else {
-			this.url = this.config.internetUrl;
-	}
-		
-		
-		
-		
-		
-		
-		
-    //    if (this.config.headerImage != '') {
-    //        image.src = this.url;
-    //        image.className = "photo";
-    //    }
+        if (this.config.headerImage != '') {
+            image.src = this.url;
+            image.className = "photo";
+        }
 
         wrapper.appendChild(image);
         return wrapper;
